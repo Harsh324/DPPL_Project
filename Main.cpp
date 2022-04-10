1b20f96c0ff1bde36d44506fd79d97da71891498
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 <harshravi324@gmail.com>
2022-04-10 08:55:22 +0530

    Driver file added

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#include"Memory_management.h"
+#include<iostream>
+
+using namespace std;
+using namespace Memory;
+
+
+int main()
+{
+    Memory_Management MM = Memory_Management(Next, 500);
+    void* One = MM.Allocate(50);
+    void* Two = MM.Allocate(100);
+    //MM.print_memory();
+    void* Three = MM.Allocate(50);
+    //MM.print_memory();
+    MM.Deallocate(Two);
+    MM.Deallocate(One);
+    void* Four = MM.Allocate(100);
+    //MM.print_memory();
+
+}
